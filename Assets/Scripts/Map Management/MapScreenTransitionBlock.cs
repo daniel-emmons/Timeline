6a142f0250f65d9d9673199077834ea82e0d7730
epolekoff <epolekoff@gmail.com>
2018-04-11 21:00:39 -0700

    Began creating the Map system. The map object has Templates and Transition Blocks. Templates are where Screens are spawned, which contain the loaded assets for a section of the map. This allows a designer to section off parts of the map by hand in the editor. Hopefully this is easy to manage.
    So far, the transitions don't do anything. The next step is to get the camera to stop at the edge of a screen and transition the player to a new screen when colliding with an edge.

---
+++ Assets/Scripts/Map Management/MapScreenTransitionBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MapScreenTransitionBlockOrientation
+{
+    Horizontal,
+    Vertical
+};
+
+public class MapScreenTransitionBlock : MonoBehaviour
+{
+
+    public MapScreenTransitionBlockOrientation Orientation;
+
+    public MapScreenTemplate LeftOrTopMapScreenTemplate;
+    public MapScreenTemplate RightOrBottomMapScreenTemplate;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
